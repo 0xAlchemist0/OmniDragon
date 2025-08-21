54a8466b40cc9f22aa9f205a383f2d66bac8118d
ExoticSolutions <1exoticeditz@gmail.com>
2025-08-21 17:46:31 -0400

    clean

--- frontend/src/Layout.tsx
+++ frontend/src/Layout.tsx
@@ -1,12 +1,16 @@
 import { Outlet } from "react-router-dom";
 import NavigatorTab from "./components/NavigatorTab";
+import TxServiceProvider from "./state/TxServiceProvider";
 
 function Layout() {
+  //fix bug here when using the provider check bugs remmebr we dont pass in the real objects we need to pass
   return (
-    <div className="text-accent-pink">
-      <NavigatorTab />
-      <Outlet />
-    </div>
+    <TxServiceProvider userInfo={{ account: "0x123", chainId: "146" }}>
+      <div className="text-accent-pink">
+        <NavigatorTab />
+        <Outlet />
+      </div>
+    </TxServiceProvider>
   );
 }
 
